0e2cfd3d9368c5e4676a4f8defb760893a77415c
lyuuhao <117192@runjian.com>
2025-12-23 16:14:16 +0800

    create posts table

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from typing import Optional
 from app import db
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from datetime import datetime, timezone
 
 
 class User(db.Model):
@@ -10,5 +11,20 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+
     def __repr__(self) -> str:
         return f"<User {self.username}>"
+
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr__(self) -> str:
+        return f"<Post {self.body}>"
