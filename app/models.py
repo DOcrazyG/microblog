ca029528e0a7839bf02a895a92c78013379dbdfc
lyuuhao <117192@runjian.com>
2025-12-24 09:43:12 +0800

    complete login

--- app/models.py
+++ app/models.py
@@ -3,18 +3,27 @@ from app import db
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
 
     def __repr__(self) -> str:
         return f"<User {self.username}>"
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -24,7 +33,12 @@ class Post(db.Model):
         index=True, default=lambda: datetime.now(timezone.utc)
     )
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
-    author: so.Mapped[User] = so.relationship(back_populates='posts')
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
 
     def __repr__(self) -> str:
         return f"<Post {self.body}>"
+
+
+@login.user_loader
+def load_user(id: str) -> User | None:
+    return db.session.get(User, int(id))
