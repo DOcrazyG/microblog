fc1046c6cc31387e767ad17e6b592e33c9197a03
lyuuhao <117192@runjian.com>
2025-12-25 16:09:19 +0800

    make it esaier to find user to  follow

--- app/routes.py
+++ app/routes.py
@@ -134,6 +134,14 @@ def unfollow(username):
         return redirect(url_for("index"))
 
 
+@app.route("/explore")
+@login_required
+def explore():
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.session.scalars(query).all()
+    return render_template("index.html", title="Explore", posts=posts)
+
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
