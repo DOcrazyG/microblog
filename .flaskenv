7f512d9cab781cd8531461075c04bded378b1f83
lyuuhao <117192@runjian.com>
2025-12-25 09:09:16 +0800

    send errors by email

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
+# flask
 FLASK_APP=microblog.py
-DATABASE_URL=sqlite:////home/lyuuhao/code/microblog/app.db
 FLASK_DEBUG=0 # set to 1 to enable debugging
--- config.py
+++ config.py
@@ -1,14 +1,25 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SECRET_KEY = os.getenv("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "sqlite:///" + os.path.join(
+        basedir, "app.db"
+    )
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = int(os.getenv("MAIL_PORT") or 25)
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") is not None
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    ADMINS = ["you_email@example.com"]
 
 
 if __name__ == "__main__":
-    print(Config.SQLALCHEMY_DATABASE_URI)
+    print(Config.SECRET_KEY)
+    print(Config.MAIL_SERVER)
