23eb4acd9e01ff4cbb15ecd77620d1607ff0e1e9
lyuuhao <117192@runjian.com>
2025-12-25 11:17:48 +0800

    get followed's posts

--- app/models.py
+++ app/models.py
@@ -78,6 +78,18 @@ class User(UserMixin, db.Model):
         )
         return db.session.scalar(query)
 
+    def followed_posts(self):
+        Author = so.aliased(User)
+        Follower = so.aliased(User)
+        return (
+            sa.select(Post)
+            .join(Post.Author.of_type(Author))
+            .join(Author.following.of_type(Follower), isouter=True)
+            .where(sa.or_(Follower.id == self.id, Author.id == self.id))
+            .group_by(Post)
+            .order_by(Post.timestamp.desc())
+        )
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
