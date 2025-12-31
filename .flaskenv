9cf709199c42b992895e10accb5165f4687c6c38
lyuuhao <117192@runjian.com>
2025-12-31 09:52:32 +0800

    reset password by email

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,3 @@
 # flask
 FLASK_APP=microblog.py
-FLASK_DEBUG=1 # set to 1 to enable debugging
+FLASK_DEBUG=0 # set to 1 to enable debugging
--- config.py
+++ config.py
@@ -12,14 +12,14 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "sqlite:///" + os.path.join(
         basedir, "app.db"
     )
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 3
 
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT") or 25)
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") is not None
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    ADMINS = ["lyuuhao@gmail.com"]
+    ADMINS = ["1379825060@qq.com"]
 
 
 if __name__ == "__main__":
